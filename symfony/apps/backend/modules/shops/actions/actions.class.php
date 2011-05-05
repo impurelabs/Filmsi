2cca5e8f84649133bb68c7fb0d14ae5d11abb69a
Iulian Manea <iulian.manea@impurelabs.com>
2011-05-05 19:23:12 +0300

    backend shops import bug

--- symfony/apps/backend/modules/shops/actions/actions.class.php
+++ symfony/apps/backend/modules/shops/actions/actions.class.php
@@ -115,6 +115,8 @@ class shopsActions extends sfActions
 	public function executeImport(sfWebRequest $request)
 	{
 		$this->forward404If(false == $this->shop = Doctrine_Core::getTable('Shop')->findOneById($request->getParameter('sid')));
+				
+		$GLOBALS['shopId'] = $this->shop->getId();
 
 		
 		if ($request->isMethod('post')){
@@ -124,6 +126,25 @@ class shopsActions extends sfActions
 			set_time_limit(10000);
 			
 			
+			
+			
+			
+			$parser = xml_parser_create();
+			xml_set_element_handler($parser, 'start_element', 'end_element');
+			xml_set_character_data_handler($parser, 'characted_data');
+			
+			$fp = fopen($request->getParameter('import_url'), 'r') or die('cannot open feed url');
+			
+			while ($data = fread($fp, 4096)) {
+				xml_parse($parser, $data, feof($fp)) or die('parsing error');
+			}
+			
+			
+			
+			
+			
+			
+			exit;
 			$products = array();
 			$importedImdbCodes = array();
 			
@@ -133,62 +154,72 @@ class shopsActions extends sfActions
 				die("Failed to open feed");
 			}
 			
+			
+			
 			while($reader->read())
 			{
 				$node = $reader->expand();
+				unset($node);
 				
 				if ($node->tagName == 'product' && $node->getAttribute('imdb') != ''){
 					
 					/* If the product exists in the database, att the film to the shop */
 					sfContext::getInstance()->getLogger()->info('Memory before: ' . $node->getAttribute('imdb') . '|' . memory_get_usage(true));
-					$film = FilmTable::getInstance()->findOneByImdbForShopImport($node->getAttribute('imdb'));
 					
-					if ($film){
-						if ($node->getAttribute('is_dvd') == '1'){
-							sfContext::getInstance()->getLogger()->info('Mem BEFORE ' . $node->getAttribute('imdb') . ' dvd: ' . memory_get_usage(true));
-							$shopFilm = new ShopFilm();
-							$shopFilm->setShopId($this->shop->getId());
-							$shopFilm->setFilmId($film->getId());
-							$shopFilm->setUrl($node->getAttribute('dvd_url'));
-							$shopFilm->setFormat(ShopFilm::FORMAT_DVD);
-							$shopFilm->save();
-
-							$shopFilm->free();
-							unset($shopFilm);
-						}
-
-						if ($node->getAttribute('is_bluray') == '1'){
-							sfContext::getInstance()->getLogger()->info('Mem BEFORE ' . $node->getAttribute('imdb') . ' bluray: ' . memory_get_usage(true));
-							$shopFilm = new ShopFilm();
-							$shopFilm->setShopId($this->shop->getId());
-							$shopFilm->setFilmId($film->getId());
-							$shopFilm->setUrl($node->getAttribute('bluray_url'));
-							$shopFilm->setFormat(ShopFilm::FORMAT_BLURAY);
-							$shopFilm->save();
-
-							$shopFilm->free();
-							unset($shopFilm);
-							sfContext::getInstance()->getLogger()->info('Mem AFTER: ' . memory_get_usage(true));
-						}
-
-						if ($node->getAttribute('is_online') == '1'){
-							sfContext::getInstance()->getLogger()->info('Mem BEFORE ' . $node->getAttribute('imdb') . ' online: ' . memory_get_usage(true));
-							$shopFilm = new ShopFilm();
-							$shopFilm->setShopId($this->shop->getId());
-							$shopFilm->setFilmId($film->getId());
-							$shopFilm->setUrl($node->getAttribute('online_url'));
-							$shopFilm->setFormat(ShopFilm::FORMAT_ONLINE);
-							$shopFilm->save();
-
-							$shopFilm->free();
-							unset($shopFilm);
-							sfContext::getInstance()->getLogger()->info('Mem AFTER: ' . memory_get_usage(true));
-						}
+//					$q = Doctrine_Query::create()
+//						->select('f.id')
+//						->from('Film f')
+//						->where('f.imdb = ?');
+//					if ($film = $q->fetchOne(array($node->getAttribute('imdb')))) {
+//						$film->free();
+//					}
+//					$q->free();
+					
+					
+//					if ($film){
+//						if ($node->getAttribute('is_dvd') == '1'){
+//							sfContext::getInstance()->getLogger()->info('Mem BEFORE ' . $node->getAttribute('imdb') . ' dvd: ' . memory_get_usage(true));
+//							$shopFilm = new ShopFilm();
+//							$shopFilm->setShopId($this->shop->getId());
+//							$shopFilm->setFilmId($film->getId());
+//							$shopFilm->setUrl($node->getAttribute('dvd_url'));
+//							$shopFilm->setFormat(ShopFilm::FORMAT_DVD);
+//							$shopFilm->save();
+//
+//							$shopFilm->free();
+//							unset($shopFilm);
+//						}
+//
+//						if ($node->getAttribute('is_bluray') == '1'){
+//							sfContext::getInstance()->getLogger()->info('Mem BEFORE ' . $node->getAttribute('imdb') . ' bluray: ' . memory_get_usage(true));
+//							$shopFilm = new ShopFilm();
+//							$shopFilm->setShopId($this->shop->getId());
+//							$shopFilm->setFilmId($film->getId());
+//							$shopFilm->setUrl($node->getAttribute('bluray_url'));
+//							$shopFilm->setFormat(ShopFilm::FORMAT_BLURAY);
+//							$shopFilm->save();
+//
+//							$shopFilm->free();
+//							unset($shopFilm);
+//							sfContext::getInstance()->getLogger()->info('Mem AFTER: ' . memory_get_usage(true));
+//						}
+//
+//						if ($node->getAttribute('is_online') == '1'){
+//							sfContext::getInstance()->getLogger()->info('Mem BEFORE ' . $node->getAttribute('imdb') . ' online: ' . memory_get_usage(true));
+//							$shopFilm = new ShopFilm();
+//							$shopFilm->setShopId($this->shop->getId());
+//							$shopFilm->setFilmId($film->getId());
+//							$shopFilm->setUrl($node->getAttribute('online_url'));
+//							$shopFilm->setFormat(ShopFilm::FORMAT_ONLINE);
+//							$shopFilm->save();
+//
+//							$shopFilm->free();
+//							unset($shopFilm);
+//							sfContext::getInstance()->getLogger()->info('Mem AFTER: ' . memory_get_usage(true));
+//						}
 						
-						$film->free();
-					}
-					
-					unset($film);
+						
+//					}
 				}
 			}
 			$reader->close();
@@ -201,4 +232,85 @@ class shopsActions extends sfActions
 			exit;
 		}
 	}
+}
+
+
+function start_element($parser, $elementName, $elementAttrs)
+{
+	if ($elementName != 'PRODUCT' || $elementAttrs['IMDB'] == ''){
+		return;
+	}
+		
+	/* If the product exists in the database, att the film to the shop */
+	echo '<br />' . 'Memory 245: ' . $elementAttrs['IMDB'] . '|' . memory_get_usage(true);
+
+	$q = Doctrine_Query::create()
+		->select('f.id')
+		->from('Film f')
+		->where('f.imdb = ?');
+	echo '<br />' . 'Memory 251: ' . $elementAttrs['IMDB'] . '|' . memory_get_usage(true);
+	if ($film = $q->fetchOne(array($elementAttrs['IMDB']))){
+		$film->free();
+	}
+	echo '<br />' . 'Memory 255: ' . $elementAttrs['IMDB'] . '|' . memory_get_usage(true);
+	$q->free();
+	
+	echo '<br />' . 'Memory 260: ' . $elementAttrs['IMDB'] . '|' . memory_get_usage(true);
+	
+	
+	
+
+//	if ($film = $q->fetchOne(array($elementAttrs['IMDB']))){
+//		if ($elementAttrs['IS_DVD'] == '1'){
+//			$shopFilm = new ShopFilm();
+//			$shopFilm->setShopId($GLOBALS['shopId']);
+//			$shopFilm->setFilmId($film->getId());
+//			$shopFilm->setUrl($elementAttrs['DVD_URL']);
+//			$shopFilm->setFormat(ShopFilm::FORMAT_DVD);
+//			$shopFilm->save();
+//
+//			$shopFilm->free();
+//			unset($shopFilm);
+//		}
+//
+//		if ($elementAttrs['IS_BLURAY'] == '1'){
+//			$shopFilm = new ShopFilm();
+//			$shopFilm->setShopId($GLOBALS['shopId']);
+//			$shopFilm->setFilmId($film->getId());
+//			$shopFilm->setUrl($elementAttrs['BLURAY_URL']);
+//			$shopFilm->setFormat(ShopFilm::FORMAT_BLURAY);
+//			$shopFilm->save();
+//
+//			$shopFilm->free();
+//			unset($shopFilm);
+//			sfContext::getInstance()->getLogger()->info('Mem AFTER: ' . memory_get_usage(true));
+//		}
+//
+//		if ($elementAttrs['IS_ONLINE'] == '1'){
+//			$shopFilm = new ShopFilm();
+//			$shopFilm->setShopId($GLOBALS['shopId']);
+//			$shopFilm->setFilmId($film->getId());
+//			$shopFilm->setUrl($elementAttrs['ONLINE_URL']);
+//			$shopFilm->setFormat(ShopFilm::FORMAT_ONLINE);
+//			$shopFilm->save();
+//
+//			$shopFilm->free();
+//			unset($shopFilm);
+//			sfContext::getInstance()->getLogger()->info('Mem AFTER: ' . memory_get_usage(true));
+//		}
+		
+//		$film->free();
+		
+//	}
+	
+//	$q->free();
+
+}
+
+function end_element($parser, $elementName){
+	
+}
+
+function character_data($parser, $data){
+	
 }

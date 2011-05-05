8c3d2e9ffbbb50abb358c2b2c5f86c17f0152831
Iulian Manea <iulian.manea@impurelabs.com>
2011-05-05 15:11:49 +0300

    backend shops import bug

--- symfony/apps/backend/modules/shops/actions/actions.class.php
+++ symfony/apps/backend/modules/shops/actions/actions.class.php
@@ -140,9 +140,9 @@ class shopsActions extends sfActions
 			
 			sfContext::getInstance()->getLogger()->info('Mem AFTER films from db: ' . memory_get_usage(true));
 			
-			for ($i = 0; $i<= $products->product->count() - 1; $i++) {	
-				//$productImdb = (string)$products->product[$i]['imdb'];
-				sfContext::getInstance()->getLogger()->info('Mem after ' . (string)$products->product[$i]['imdb'] . '|' . memory_get_usage(true));
+			foreach ($products->product as $product) {	
+				$productImdb = (string)$product['imdb'];
+				sfContext::getInstance()->getLogger()->info('Mem after ' . $productImdb . memory_get_usage(true));
 				
 				
 				/* Check if the product exists in the database */

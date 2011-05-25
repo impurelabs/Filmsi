eed2c0c1af8d5cea8181ca32830911e75a629e88
Iulian Manea <iulian.manea@impurelabs.com>
2011-05-25 10:55:39 +0300

    fixing wired backed bug

--- public_html/backend_dev.php
+++ public_html/backend_dev.php
@@ -1,5 +1,5 @@
 <?php
-
+die('asd');
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
 // feel free to remove this, extend it or make something more sophisticated.
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '78.97.152.233', '::1')))
@@ -8,6 +8,6 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '78.97.152.233', '::1
 }
 
 require_once(dirname(__FILE__).'/../symfony/config/ProjectConfiguration.class.php');
-die('asd');
+
 $configuration = ProjectConfiguration::getApplicationConfiguration('backend', 'dev', true);
 sfContext::createInstance($configuration)->dispatch();

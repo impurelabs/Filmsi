4d6b416b92ff4c7e55d773df7dea35580a05eb00
Iulian Manea <iulian.manea@impurelabs.com>
2011-06-06 17:45:40 +0300

    Frontend bug

--- symfony/apps/frontend/modules/default/templates/indexSuccess.php
+++ symfony/apps/frontend/modules/default/templates/indexSuccess.php
@@ -502,10 +502,10 @@
     <div class="cell-bd" style="height: 162px">
 		<?php foreach($filmPhotos  as $filmPhoto):?>
 			<div class="inline-block align-center spacer-bottom ml-1" style="width: 85px; vertical-align: top">
-				<a href="<?php echo url_for('@film_photos?id=' . $filmPhoto->getAlbum()->getFilm()->getId() . '&key=' . $filmPhoto->getAlbum()->getFilm()->getUrlKey());?>?pid=<?php echo $filmPhoto->getId();?>">
+				<a href="<?php echo url_for('@film_photos?id=' . $filmPhoto->getAlbum()->getFilm()->getId() . '&key=' . $filmPhoto->getAlbum()->getFilm()->getUrlKey());?>?photo_id=<?php echo $filmPhoto->getId();?>">
 					<img width="80" src="<?php echo filmsiPhotoThumb($filmPhoto->getFilename());?>" />
 				</a> <br />
-				<a href="<?php echo url_for('@film_photos?id=' . $filmPhoto->getAlbum()->getFilm()->getId() . '&key=' . $filmPhoto->getAlbum()->getFilm()->getUrlKey());?>?pid=<?php echo $filmPhoto->getId();?>" class="black-link">
+				<a href="<?php echo url_for('@film_photos?id=' . $filmPhoto->getAlbum()->getFilm()->getId() . '&key=' . $filmPhoto->getAlbum()->getFilm()->getUrlKey());?>?photo_id=<?php echo $filmPhoto->getId();?>" class="black-link">
 					<?php echo $filmPhoto->getAlbum()->getFilm()->getNameRo();?>
 				</a> <br />
 			</div>

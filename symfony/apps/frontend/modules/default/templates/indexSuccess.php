7521d276ff70856dc4ef08821b150ce793cb7c4a
Iulian Manea <iulian.manea@impurelabs.com>
2011-06-06 15:51:41 +0300

    Frontend bug

--- symfony/apps/frontend/modules/default/templates/indexSuccess.php
+++ symfony/apps/frontend/modules/default/templates/indexSuccess.php
@@ -248,37 +248,14 @@
         </div>
         <div class="cell-bd" style="height: 470px">
           <?php foreach ($filmsSoonInCinema as $filmSoonInCinema):?>
-				<div class="mb-3" style="position: relative">
-					<div class="inline-block spacer-right-s details-container" style="vertical-align:top">
-						<a href="<?php echo url_for('@film?id=' . $filmSoonInCinema->getId() . '&key=' . $filmSoonInCinema->getUrlKey());?>"><img src="<?php echo filmsiFilmPhotoThumbS($filmSoonInCinema->getFilename());?>" style="width: 49px" /></a>
-						<div class="detailscell" id="detailscell4-<?php echo $filmSoonInCinema->getId();?>"> <a href="<?php echo url_for('@film?id=' . $filmSoonInCinema->getId() . '&key=' . $filmSoonInCinema->getUrlKey());?>" class="left"><img width="100" src="<?php echo filmsiFilmPhotoThumb($filmSoonInCinema->getFilename());?>" /></a>
-						  <div class="details-content"> <a href="<?php echo url_for('@film?id=' . $filmSoonInCinema->getId() . '&key=' . $filmSoonInCinema->getUrlKey());?>" class="bigblack-link"><?php echo $filmSoonInCinema->getNameRo();?></a> <br />
-							<?php if($filmSoonInCinema->getNameEn() != ''):?><em>(<?php echo $filmSoonInCinema->getNameEn();?>)</em><?php endif;?>
-							<hr class="cell-separator1 spacer-top spacer-bottom" />
-							<?php if (count($filmSoonInCinema->getBestDirectors()) > 0):?>
-							<span class="explanation-small">Regia:</span>
-								<?php foreach ($filmSoonInCinema->getBestDirectors() as $person):?>
-									<a href="<?php echo url_for('@person?id=' . $person->getId() . '&key=' . $person->getUrlKey());?>" class="explanation-link"><?php echo $person->getName();?></a>,
-								<?php endforeach;?>
-							<?php endif;?>
-							<span class="explanation-small">Cu:</span>
-								<?php foreach ($filmSoonInCinema->getBestActors(3) as $person):?>
-									<a href="<?php echo url_for('@person?id=' . $person->getId() . '&key=' . $person->getUrlKey());?>" class="explanation-link"><?php echo $person->getName();?></a>,
-								<?php endforeach;?>
-							<hr class="cell-separator1 spacer-top spacer-bottom" />
-							<a href="<?php echo url_for('@cinema_search');?>?f=<?php echo $filmSoonInCinema->getId();?>" class="whitebutton-small-link">rezerva bilet&raquo;</a>
-							<a href="<?php echo url_for('@film_videos?id=' . $filmSoonInCinema->getId() . '&key=' . $filmSoonInCinema->getUrlKey());?>" class="whitebutton-small-link spacer-left">vezi trailer &raquo;</a> </div>
-						  <span class="details-cioc"></span> </div>
-						<!-- details end -->
-					</div>
-				<div class="inline-block cell-separator-dotted-bottom" style="width: 200px"> <a href="<?php echo url_for('@film?id=' . $filmSoonInCinema->getId() . '&key=' . $filmSoonInCinema->getUrlKey());?>" class="important-link"><?php echo $filmSoonInCinema->getNameRo();?></a><br />
+				<div class="inline-block cell-separator-dotted-bottom mb-3"> 
+					<a href="<?php echo url_for('@film?id=' . $filmSoonInCinema->getId() . '&key=' . $filmSoonInCinema->getUrlKey());?>" class="important-link"><?php echo $filmSoonInCinema->getNameRo();?></a><br />
 				  <?php if($filmSoonInCinema->getNameEn() != ''):?><em>(<?php echo $filmSoonInCinema->getNameEn();?>)</em><?php endif;?>
 				  <div class="spacer-top-sm explanation-small">Cu:
 					  <?php foreach ($filmSoonInCinema->getBestActors(3) as $person):?>
 						<a href="<?php echo url_for('@person?id=' . $person->getId() . '&key=' . $person->getUrlKey());?>" class="explanation-link"><?php echo $person->getName();?></a>,
 					<?php endforeach;?>
 				  </div>
-				</div>
 				</div>
 			<?php endforeach;?>
         </div>

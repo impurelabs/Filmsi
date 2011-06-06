231b4d250dc999078271248f2a1642c1be63f6fd
Iulian Manea <iulian.manea@impurelabs.com>
2011-06-06 18:08:13 +0300

    Frontend bug

--- symfony/apps/frontend/modules/gadgets/templates/gadget2Success.php
+++ symfony/apps/frontend/modules/gadgets/templates/gadget2Success.php
@@ -2,12 +2,12 @@
 <link rel="stylesheet" type="text/css" media="screen" href="<?php echo stylesheet_path('main.css', true);?>" />
 <div class="cell" style="height:518px">
 	<div class="cell-hd">
-		<h5>Nou pe <span class="black">DVD &amp; Bluray</span></h5>
+		<h5>Acum pe <span class="black">DVD &amp; Bluray</span></h5>
 	</div>
 	<div class="cell-bd">
 		<?php foreach($films as $film):?>
 			<div class="mb-3 details-container">
-				<div class="inline-block spacer-right-s" style="vertical-align:top"><a href="<?php echo url_for('@film?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>"><img src="<?php echo filmsiFilmPhotoThumbS($film->getFilename());?>" /></a></div>
+				<div class="inline-block spacer-right-s" style="vertical-align:top"><a href="<?php echo url_for('@film?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>"><img src="<?php echo filmsiFilmPhotoThumbS($film->getFilename());?>" style="width: 50px;" /></a></div>
 				<div class="inline-block cell-separator-dotted-bottom" style="width: 200px"> <a href="<?php echo url_for('@film?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>" class="important-link"><?php echo $film->getNameRo();?></a><br />
 				  <?php if ($film->getNameEn() != ''):?><em>(<?php echo $film->getNameEn();?>)</em><?php endif;?>&nbsp;
 				  <div class="spacer-top-sm explanation-small">Cu:

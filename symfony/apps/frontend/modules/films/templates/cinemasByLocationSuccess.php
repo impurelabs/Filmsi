5c1717d54bac18db402d68f4db80b0ebd293f09c
Iulian Manea <iulian.manea@impurelabs.com>
2011-06-07 14:15:34 +0300

    Iss26 take2

--- symfony/apps/frontend/modules/films/templates/cinemasByLocationSuccess.php
+++ symfony/apps/frontend/modules/films/templates/cinemasByLocationSuccess.php
@@ -1,15 +1,15 @@
 <?php foreach ($cinemaSchedules as $cinemaSchedule):?>
-<div class="cell-separator-double pb-2 mb-2">
-	<a href="<?php echo url_for('@cinema?id=' . $cinemaSchedule['id'] . '&key=' . $cinemaSchedule['url_key']);?>" class="bigblue-link"><?php echo $cinemaSchedule['name'];?></a><br />
-	
-	<table>
-	<?php foreach($cinemaSchedule['Schedule'] as $schedule):?>
-		<tr>
-			<td><?php echo filmsiDayOfWeek(date('N'), strtotime($schedule['day']));?></td>
-			<td><?php echo $schedule['schedule'];?></td>
-			<td><?php if ($cinemaSchedule['reservation_url'] != ''):?><a href="<?php echo $cinemaSchedule['reservation_url'];?>" target="_blank" class="greenbutton-s-link">Rezerva</a><?php endif;?></td>
-		</tr>
-	<?php endforeach; ?>
-	</table>
-</div>
+<a href="<?php echo url_for('@cinema?id=' . $cinemaSchedule['id'] . '&key=' . $cinemaSchedule['url_key']);?>" class="bigblue-link"><?php echo $cinemaSchedule['name'];?></a><br />
+
+<table>
+<?php foreach($cinemaSchedule['Schedule'] as $schedule):?>
+	<tr>
+		<td><?php echo filmsiDayOfWeek(date('N'), strtotime($schedule['day']));?></td>
+		<td><?php echo $schedule['schedule'];?></td>
+		<td><?php if ($cinemaSchedule['reservation_url'] != ''):?><a href="<?php echo $cinemaSchedule['reservation_url'];?>" target="_blank" class="greenbutton-s-link">Rezerva</a><?php endif;?></td>
+	</tr>
+<?php endforeach; ?>
+</table>
+
+<hr class="cell-separator-double pb-2 mb-2" />
 <?php endforeach; ?>

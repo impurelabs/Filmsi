f0d064090bb7f659ebf39901bf8f2d8ecf28a594
Iulian Manea <iulian.manea@impurelabs.com>
2011-06-08 00:08:23 +0300

    some bug

--- symfony/apps/frontend/modules/films/templates/nowInCinemaSuccess.php
+++ symfony/apps/frontend/modules/films/templates/nowInCinemaSuccess.php
@@ -116,8 +116,13 @@
         	<div class="innerspacer-bottom-s spacer-bottom-s cell-separator-dotted-bottom">
                 <a href="<?php echo url_for('@film?id=' . $film['id'] . '&key=' . $film['url_key']);?>"><img src="<?php echo filmsiFilmPhotoThumb($film['filename']);?>" style="width: 136px" /></a>
 				<div class="mb-2"></div>
-				<?php if ($film['status_cinema_year'] != '' && $film['status_cinema_month'] && $film['status_cinema_day'] != ''):?>
-					<span class="smalltext" style="color: #d70000"><?php echo format_date($film['status_cinema_year'] . '-' . $film['status_cinema_month'] . '-' . $film['status_cinema_day'], 'd', 'ro');?></span>
+				<?php if (
+							$film['status_cinema_year'] != '' && 
+							$film['status_cinema_month'] && 
+							$film['status_cinema_day'] != '' && 
+						(int)strtotime($film['status_cinema_year'] . '-' . $film['status_cinema_month'] . '-' . $film['status_cinema_day']) > strtotime('last Monday')
+						):?>
+					<div class="smalltext align-center" style="color: #d70000"><?php echo format_date($film['status_cinema_year'] . '-' . $film['status_cinema_month'] . '-' . $film['status_cinema_day'], 'd', 'ro');?></div>
 				<?php endif;?>
                 <a href="<?php echo url_for('@film?id=' . $film['id'] . '&key=' . $film['url_key']);?>" class="important-link"><?php echo $film['name_ro'];?></a>
         	</div>

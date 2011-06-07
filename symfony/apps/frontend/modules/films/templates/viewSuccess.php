23c6e9725fe4e82b1bf0b8cb3f4972bcb743bf71
Iulian Manea <iulian.manea@impurelabs.com>
2011-06-07 16:57:45 +0300

    frontend bug

--- symfony/apps/frontend/modules/films/templates/viewSuccess.php
+++ symfony/apps/frontend/modules/films/templates/viewSuccess.php
@@ -323,16 +323,16 @@
 			
 			<div style="display: inline-block; width: 140px; vertical-align: top">
 				<?php if ($film->isOnDvd() || $film->isOnBluray() || $film->isOnline()):?>
-				<span style="color: #51b15c; font-size: 20px">&#8226</span><a href="<?php echo url_for('@film_buy?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>">Cumpara DVD & Bluray</a>
+				<span style="color: #51b15c; font-size: 20px; margin-right: 5px; vertical-align: middle">&#8226</span><a href="<?php echo url_for('@film_buy?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>">Cumpara DVD & Bluray</a>
 				<div class="mb-1"></div>
 				<?php endif;?>
 				<?php if ($film->isInCinema()):?>
-				<span style="color: #51b15c; font-size: 20px">&#8226</span><a href="<?php echo url_for('@film_tickets?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>">Rezerva bilete</a>
+				<span style="color: #51b15c; font-size: 20px; margin-right: 5px; vertical-align: middle">&#8226</span><a href="<?php echo url_for('@film_tickets?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>">Rezerva bilete</a>
 				<div class="mb-1"></div>
 				<?php endif;?>
-				<span style="color: #51b15c; font-size: 20px">&#8226</span><a href="<?php echo url_for('@film_cast?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>">Actori & echipa<span class="filter-cioc"></span></a>
+				<span style="color: #51b15c; font-size: 20px; margin-right: 5px; vertical-align: middle">&#8226</span><a href="<?php echo url_for('@film_cast?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>">Actori & echipa<span class="filter-cioc"></span></a>
 				<div class="mb-1"></div>
-				<span style="color: #51b15c; font-size: 20px">&#8226</span><a href="<?php echo url_for('@film_sinopsis?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>">Sinopsis<span class="filter-cioc"></span></a>
+				<span style="color: #51b15c; font-size: 20px; margin-right: 5px; vertical-align: middle">&#8226</span><a href="<?php echo url_for('@film_sinopsis?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>">Sinopsis<span class="filter-cioc"></span></a>
 			</div>
 			<div style="display: inline-block; width: 140px; vertical-align: top; margin-left: 10px">
 				

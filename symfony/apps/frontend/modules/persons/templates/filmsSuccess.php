41a4b22c2479e69450f74bc7cc3e5ceb7d21938b
Iulian Manea <iulian.manea@impurelabs.com>
2011-06-07 22:56:18 +0300

    some bug

--- symfony/apps/frontend/modules/persons/templates/filmsSuccess.php
+++ symfony/apps/frontend/modules/persons/templates/filmsSuccess.php
@@ -35,24 +35,26 @@
     <div class="normalcell spacer-bottom tinyMce">
         <h4 class="spacer-bottom-m">Filmografie</h4>
 
+		
+		
         <?php foreach($sf_data->getRaw('films') as $film):?>
-		<div class="right align-right">
-			<table>
-			<?php foreach($film->getShops() as $shop):?>
-				<tr>
-					<td><a href="<?php echo $shop['url'];?>"><img src="<?php echo filmsiShopPhotoThumbS($shop['filename']);?>" /></a></td>
-					<td valign="middle" align="left"><a href="<?php echo $shop['url'];?>"><?php echo $shop['name'];?></a></td>
-				</tr>
-			<?php endforeach;?>
-			</table>
-		</div>
-        <div class="align-center mb-3" style="width: 110px">
-            <a href="<?php echo url_for('@film?id=' . $film['id'] . '&key=' . $film['url_key']);?>" class="important-link">
-                <img src="<?php echo filmsiFilmPhotoThumb($film['filename']);?>" style="width: 110px; border: 1px solid #d5d5d5; padding: 1px" /><br /><?php echo $film['name_en'];?>
-            </a><br />
-            <em>( <?php echo $film['name_ro'];?>)</em>
-        </div>
-		<div class="clear"></div>
+			<div class="align-center mb-3" style="display: inline-block; width: 110px">
+				<a href="<?php echo url_for('@film?id=' . $film['id'] . '&key=' . $film['url_key']);?>" class="important-link">
+					<img src="<?php echo filmsiFilmPhotoThumb($film['filename']);?>" style="width: 110px; border: 1px solid #d5d5d5; padding: 1px" /><br /><?php echo $film['name_en'];?>
+				</a><br />
+				<em>( <?php echo $film['name_ro'];?>)</em>
+			</div>
+			<div class="ml-2" style="display: inline-block; width: 340px">
+				<pre><?php var_dump($film->getShops());?></pre>
+				<!--<table>
+				<?php foreach($film->getShops() as $shop):?>
+					<tr>
+						<td><a href="<?php echo $shop['url'];?>"><img src="<?php echo filmsiShopPhotoThumbS($shop['filename']);?>" /></a></td>
+						<td valign="middle" align="left"><a href="<?php echo $shop['url'];?>"><?php echo $shop['name'];?></a></td>
+					</tr>
+				<?php endforeach;?>
+				</table>-->
+			</div>
         <?php endforeach;?>
 
         <div class="clear"></div>

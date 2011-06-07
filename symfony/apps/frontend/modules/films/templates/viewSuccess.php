e37b4c678b28aebc2c312cd93e57cac54c7d741b
Iulian Manea <iulian.manea@impurelabs.com>
2011-06-07 17:03:27 +0300

    frontend bug

--- symfony/apps/frontend/modules/films/templates/viewSuccess.php
+++ symfony/apps/frontend/modules/films/templates/viewSuccess.php
@@ -323,22 +323,37 @@
 			
 			<div style="display: inline-block; width: 140px; vertical-align: top">
 				<?php if ($film->isOnDvd() || $film->isOnBluray() || $film->isOnline()):?>
-				<span style="color: #51b15c; font-size: 20px; margin-right: 5px; vertical-align: middle">&#8226</span><a href="<?php echo url_for('@film_buy?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>">Cumpara DVD & Bluray</a>
+				<span class="film-bottom-menu-bullet">&#8226</span><a href="<?php echo url_for('@film_buy?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>">Cumpara DVD & Bluray</a>
 				<div class="mb-1"></div>
 				<?php endif;?>
 				<?php if ($film->isInCinema()):?>
-				<span style="color: #51b15c; font-size: 20px; margin-right: 5px; vertical-align: middle">&#8226</span><a href="<?php echo url_for('@film_tickets?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>">Rezerva bilete</a>
+				<span class="film-bottom-menu-bullet">&#8226</span><a href="<?php echo url_for('@film_tickets?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>">Rezerva bilete</a>
 				<div class="mb-1"></div>
 				<?php endif;?>
-				<span style="color: #51b15c; font-size: 20px; margin-right: 5px; vertical-align: middle">&#8226</span><a href="<?php echo url_for('@film_cast?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>">Actori & echipa<span class="filter-cioc"></span></a>
+				<span class="film-bottom-menu-bullet">&#8226</span><a href="<?php echo url_for('@film_cast?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>">Actori & echipa<span class="filter-cioc"></span></a>
 				<div class="mb-1"></div>
-				<span style="color: #51b15c; font-size: 20px; margin-right: 5px; vertical-align: middle">&#8226</span><a href="<?php echo url_for('@film_sinopsis?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>">Sinopsis<span class="filter-cioc"></span></a>
+				<span class="film-bottom-menu-bullet">&#8226</span><a href="<?php echo url_for('@film_sinopsis?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>">Sinopsis<span class="filter-cioc"></span></a>
 			</div>
 			<div style="display: inline-block; width: 140px; vertical-align: top; margin-left: 10px">
-				
+				<span class="film-bottom-menu-bullet">&#8226</span><a href="<?php echo url_for('@film_awards?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>">Premii<span class="filter-cioc"></span></a>
+				<div class="mb-1"></div>
+				<span class="film-bottom-menu-bullet">&#8226</span><a href="<?php echo url_for('@film_articles?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>">Articole despre film<span class="filter-cioc"></span></a>
+				<div class="mb-1"></div>
+				<span class="film-bottom-menu-bullet">&#8226</span><a href="<?php echo url_for('@film_comments?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>">Comentariile publicului<span class="filter-cioc"></span></a>
+				<div class="mb-1"></div>
+				<?php if ($film->hasNonRedcarpetPhotos()):?>
+				<span class="film-bottom-menu-bullet">&#8226</span><a href="<?php echo url_for('@film_photos?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>">Fotografii din film<span class="filter-cioc"></span></a>
+				<div class="mb-1"></div>
+				<?php endif;?>
 			</div>
-			<div style="display: inline-block; width: 140px; vertical-align: top margin-left: 10px">
-				
+			<div style="display: inline-block; width: 140px; vertical-align: top; margin-left: 10px">
+				<?php if ($film->hasRedcarpetPhotos()):?>
+				<span class="film-bottom-menu-bullet">&#8226</span><a href="<?php echo url_for('@film_redcarpet?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>">Fotografii de la premiera<span class="filter-cioc"></span></a>
+				<div class="mb-1"></div>
+				<?php endif;?>
+				<span class="film-bottom-menu-bullet">&#8226</span><a href="<?php echo url_for('@film_videos?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>">Trailere si clipuri<span class="filter-cioc"></span></a>
+				<div class="mb-1"></div>
+				<span class="film-bottom-menu-bullet">&#8226</span><a href="<?php echo url_for('@film_stiri?id=' . $film->getId() . '&key=' . $film->getUrlKey());?>">Stiri<span class="filter-cioc"></span></a>
 			</div>
 			
 			

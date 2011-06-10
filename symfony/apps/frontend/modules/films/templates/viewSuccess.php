964f9c7e041d450039aebf4a56cbbee51ce7a77c
Iulian Manea <iulian.manea@impurelabs.com>
2011-06-10 07:47:10 +0300

    somestuff

--- symfony/apps/frontend/modules/films/templates/viewSuccess.php
+++ symfony/apps/frontend/modules/films/templates/viewSuccess.php
@@ -36,10 +36,11 @@
     	<img src="<?php echo filmsiFilmPhotoThumb($film->getFilename());?>" class="spacer-bottom" width="185" />
 		
 		<br />
-			<a href="http://twitter.com/share" class="twitter-share-button" data-text="Uite ba ce fim mishto , pe al mai tare site <?php echo $film->getNameRo();?>" data-count="horizontal">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+			<a href="http://twitter.com/share" class="twitter-share-button" data-text="Uite ba ce fim mishto , pe al mai tare site <?php echo $film->getNameRo();?>" data-count="vertical">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
 			<div class="mb-2"></div>
-			<div id="fb-root"></div><script src="http://connect.facebook.net/en_US/all.js#appId=210613575638257&amp;xfbml=1"></script><fb:like href="" send="false" layout="button_count" width="180" show_faces="false" font=""></fb:like>
-
+			<div id="fb-root"></div><script src="http://connect.facebook.net/en_US/all.js#appId=210613575638257&amp;xfbml=1"></script><fb:like href="" send="false" layout="box_count" width="180" show_faces="false" font=""></fb:like>
+		
+		<br />
         <?php include_partial('films/alert', array('filmId' => $film->getId()));?>		
     </div>
 
